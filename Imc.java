f2bbb5b1419988b24a5576e555813d7b97de9f27
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-03-07 17:48:22 +0000

    changes_07-03

--- Imc.java
+++ Imc.java
@@ -7,15 +7,15 @@ public class Imc {
         System.out.println(imc);
 
         if (imc <= 18.5) {
-            System.out.println("Sua Classificação é de Magreza");
+            System.out.println("Sua classificação é de magreza");
         } else if (imc >= 18.5 && imc <= 24.9) {
-            System.out.println("Sua Classificação Está Dentro do Normal");
+            System.out.println("Parabéns, sua classificação está dentro do normal");
         } else if (imc >= 25.0 && imc <= 29.9) {
-            System.out.println("Sua Classificação é de Sobrepeso I");
+            System.out.println("Sua classificação é de sobrepeso I");
         } else if (imc >= 30.0 && imc <= 39.9) {
-            System.out.println("Sua Classificação é Obesidade II");
+            System.out.println("Sua classificação é obesidade II");
         } else if (imc >= 40.0 ) {
-            System.out.println("Sua Classificação é Obesidade Grave III");
+            System.out.println("Sua classificação é obesidade grave III");
             
         }
     }
